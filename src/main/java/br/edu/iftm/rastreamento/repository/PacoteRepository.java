db78f7adb0621fd8b7cf2e8b0f7a66d3326af9ee
Ana Clara Custodio <anaccustodio8@gmail.com>
2024-09-02 20:04:01 -0300

    add metodos de busca no PacoteRepository

--- src/main/java/br/edu/iftm/rastreamento/repository/PacoteRepository.java
+++ src/main/java/br/edu/iftm/rastreamento/repository/PacoteRepository.java
@@ -1,10 +1,16 @@
 package br.edu.iftm.rastreamento.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import br.edu.iftm.rastreamento.model.Pacote;
 @Repository
 public interface PacoteRepository extends CrudRepository<Pacote, Long> {
-	
+	// Metodo para buscar pacotes pelo status
+    List<Pacote> findByStatus(String status);
+
+    // Metodo para buscar pacotes pelo destinatário
+    List<Pacote> findByDestinatario(String destinatario);
 }
