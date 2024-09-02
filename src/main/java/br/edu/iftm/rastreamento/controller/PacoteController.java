0e2a136037e42bc959b30a9244440d7630cef5ca
Ana Clara Custodio <anaccustodio8@gmail.com>
2024-09-02 19:27:33 -0300

    inserindo swagger no PacoteController

--- src/main/java/br/edu/iftm/rastreamento/controller/PacoteController.java
+++ src/main/java/br/edu/iftm/rastreamento/controller/PacoteController.java
@@ -14,6 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.iftm.rastreamento.model.Pacote;
 import br.edu.iftm.rastreamento.service.PacoteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
 @RequestMapping("/pacotes")
@@ -22,26 +27,53 @@ public class PacoteController {
 	@Autowired
 	private PacoteService pacoteService;
 
+	@Operation(summary = "Retorna todos os pacotes")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Lista de pacotes retornada com sucesso", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = Pacote.class)) }),
+			@ApiResponse(responseCode = "204", description = "Nenhum pacote foi encontrado")
+	})
 	@GetMapping
 	public List<Pacote> getAllPacotes() {
 		return pacoteService.getAllPacotes();
 	}
 
+	@Operation(summary = "Cria um novo pacote")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "201", description = "Pacote criado com sucesso", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = Pacote.class)) }),
+			@ApiResponse(responseCode = "400", description = "Houve um erro na criação do pacote", content = @Content)
+	})
 	@PostMapping
 	public Pacote createPacote(@RequestBody Pacote pacote) {
 		return pacoteService.createPacote(pacote);
 	}
 
+	@Operation(summary = "Retorna um pacote pelo ID")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "Pacote retornado com sucesso", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Pacote.class))}),
+		@ApiResponse(responseCode = "204", description = "Não foi encontrado nenhum pacote que corresponde ao ID informado", content = @Content)
+	})
 	@GetMapping("/{id}")
 	public Pacote getPacoteById(@PathVariable Long id) {
 		return pacoteService.getPacoteById(id);
 	}
 
+	@Operation(summary = "Atualiza um pacote existente pelo ID")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "Pacote atualizado com sucesso", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Pacote.class))}),
+		@ApiResponse(responseCode = "204", description = "Não foi encontrado nenhum pacote que corresponde ao ID informado", content = @Content)
+	})
 	@PutMapping("/{id}")
 	public Pacote updatePacote(@PathVariable Long id, @RequestBody Pacote pacoteDetails) {
 		return pacoteService.updatePacote(id, pacoteDetails);
 	}
 
+	@Operation(summary = "Deleta um pacote existente pelo ID")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "Pacote deletado com sucesso"),
+		@ApiResponse(responseCode = "204", description = "Não foi encontrado nenhum pacote que corresponde ao ID informado")
+	})
 	@DeleteMapping("/{id}")
 	public void deletePacote(@PathVariable Long id) {
 		pacoteService.deletePacote(id);
