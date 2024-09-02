6e9397bb20a686284587f62b5dd0f816358f340f
Ana Clara Custodio <anaccustodio8@gmail.com>
2024-09-02 19:45:51 -0300

    inserindo swagger no EnderecoController

--- src/main/java/br/edu/iftm/rastreamento/controller/EnderecoController.java
+++ src/main/java/br/edu/iftm/rastreamento/controller/EnderecoController.java
@@ -13,7 +13,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.iftm.rastreamento.dto.EnderecoDTO;
+import br.edu.iftm.rastreamento.model.Pacote;
 import br.edu.iftm.rastreamento.service.EnderecoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
 @RequestMapping("/enderecos")
@@ -22,18 +28,35 @@ public class EnderecoController {
 	@Autowired
 	private EnderecoService enderecoService;
 
+	@Operation(summary = "Retorna todos os endereços")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Lista de endereços retornada com sucesso", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = Pacote.class)) }),
+			@ApiResponse(responseCode = "204", description = "Nenhum endereço foi encontrado")
+	})
 	@GetMapping
 	public List<EnderecoDTO> getAllEnderecos() {
 		System.out.println("-----------------------------------------");
 		return enderecoService.getAllEnderecos();
 	}
 
+	@Operation(summary = "Retorna um endereço pelo ID")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "Endereço retornado com sucesso", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Pacote.class))}),
+		@ApiResponse(responseCode = "204", description = "Não foi encontrado nenhum endereço que corresponde ao ID informado", content = @Content)
+	})
 	@GetMapping("/{id}")
 	public ResponseEntity<EnderecoDTO> getById(@PathVariable Long id) {
 		EnderecoDTO endereco = enderecoService.getEnderecoById(id);
 		return ResponseEntity.status(HttpStatus.FOUND).body(endereco);
 	}
 
+	@Operation(summary = "Cria um novo endereço")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "201", description = "Endereço criado com sucesso", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = Pacote.class)) }),
+			@ApiResponse(responseCode = "400", description = "Houve um erro na criação do endereço", content = @Content)
+	})
 	@PostMapping
 	public ResponseEntity<EnderecoDTO> createEndereco(@RequestBody EnderecoDTO enderecoDTO) {
 		EnderecoDTO novoRecursEnderecoDTO = enderecoService.createEndereco(enderecoDTO);
